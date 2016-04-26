6e46ab2108cad14d7a65738c333dbc744141f765
zatizali <zatizali94@gmail.com>
2016-04-26 17:30:21 +0800

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+        <html lang="'en">
+
+    <head>
+        <title>Laravel Quickstart</title>
+    </head>
+
+<body>
+<div class="container">
+    <nav class = "navbar navbar-default">
+    </nav>
+</div>
+
+@yield('content')
+
+</body>
+</html>
